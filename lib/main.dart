815739bd7faf674bf96a9eed06c6ed00e374e2b2
YusraKhalid1 <yusrak612@gmail.com>
2023-04-18 05:17:14 -0700

    whatsapp direct-chat

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:app/views/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyClass(),
+  ));
+}
